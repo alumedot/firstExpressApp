a2540af6f4b92c937e7d952d92ab1cab8a72c921
Alexey Kozlov <alumedot@icloud.com>
2022-03-20 14:16:24 +0000

    feat: store session in the mongodb

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import session from 'express-session';
+import ConnectMongoSession from 'connect-mongodb-session';
 
 import { router as adminRoutes } from './routes/admin';
 import { router as shopRoutes } from './routes/shop';
@@ -10,7 +11,15 @@ import { router as authRoutes } from './routes/auth';
 import { get404 } from './controllers/error';
 import { User } from './models/user';
 
+const MONGODB_URI = 'mongodb+srv://alumedot:VXrg9xp82OVDerum@cluster0.2fqy4.mongodb.net/shop';
+
+const MongoDBStore = ConnectMongoSession(session);
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 
@@ -22,7 +31,12 @@ app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }));
+app.use(session({
+  secret: 'my secret',
+  resave: false,
+  saveUninitialized: false,
+  store
+}));
 
 app.use((req, res, next) => {
   User.findById('622cf70ff9500387cdc8b38b')
@@ -40,7 +54,7 @@ app.use(authRoutes);
 app.use(get404);
 
 mongoose
-  .connect('mongodb+srv://alumedot:VXrg9xp82OVDerum@cluster0.2fqy4.mongodb.net/shop?retryWrites=true')
+  .connect(MONGODB_URI)
   .then(() => {
     User.findOne().then((existingUser) => {
       if (!existingUser) {
